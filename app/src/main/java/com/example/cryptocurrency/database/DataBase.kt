b336a34b3001268a587b7c29b075fb77586376bf
Jostein Eriksen <jostein.eriksen@hotmail.com>
2021-04-11 17:08:25 +0200

    Configured the database

--- app/src/main/java/com/example/cryptocurrency/database/DataBase.kt
+++ app/src/main/java/com/example/cryptocurrency/database/DataBase.kt
@@ -1,3 +1,29 @@
 package com.example.cryptocurrency.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.Transaction
+
 const val DATABASE_NAME: String = "transactions_database"
+
+// Singleton database
+@Database(entities = [Transaction::class], version = 1) // Room handles which version
+abstract class DataBase : RoomDatabase(){
+    abstract fun getTransactionDAO() : TransactionsDAO
+
+    companion object{
+
+        var db : DataBase? = null
+
+        fun getDatabase(context: Context) : DataBase{ // Burde kanskje ha threadlocking her sånn at ikke 2 tråder kan opprette på samme tidspunkt
+
+            val newDB = db?: Room.databaseBuilder(context,
+                DataBase::class.java, DATABASE_NAME).build()
+            return newDB.also {
+                db = it
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/cryptocurrency/database/TransactionsDAO.kt
+++ app/src/main/java/com/example/cryptocurrency/database/TransactionsDAO.kt
@@ -1,4 +1,20 @@
 package com.example.cryptocurrency.database
 
-class TransactionsDAO {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Update
+import com.example.cryptocurrency.entities.Transactions
+
+@Dao
+interface TransactionsDAO{
+    @Insert
+    fun insert(transaction: Transactions)
+
+    @Update
+    fun update(transaction: Transactions)
+
+    @Delete
+    fun delete(transaction: Transactions)
+
 }
